211f37d1aac6b920a44c20d63f16d67e1f80d804
AlexanDER7012 <216101886+AlexanDER7012@users.noreply.github.com>
2025-12-22 12:45:44 -0600

    recursividad corregida

--- Recursividadd/src/main/java/com/mycompany/recursividadd/Recursividadd.java
+++ Recursividadd/src/main/java/com/mycompany/recursividadd/Recursividadd.java
@@ -2,11 +2,11 @@ package com.mycompany.recursividadd;
 
 public class Recursividadd {
 
-    public static void Recursividad(int n, int origen, int aux, int dest ){
-        if(n != 0){
-            Recursividad(n-1, origen, dest, aux);
+    public static void Recursividad(int n, int origen, int dest, int aux ){
+        if(n > 0){
+            Recursividad(n-1, origen, aux, dest);
             System.out.println("Del destino " + origen + " se movio a " + dest );
-            Recursividad(n-1, dest, aux, origen);
+            Recursividad(n-1, aux, dest, origen);
         }
         
     }
